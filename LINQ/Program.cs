a5326ef77a80db2cb82540f7a15ce4b08ea8a800
Justin Simon <justinsimoncs@gmail.com>
2025-11-26 11:07:56 -0800

    Added instructions for project

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace LINQ
+﻿using System.Runtime.Intrinsics.X86;
+
+namespace LINQ
 {
     public class Program
     {
@@ -8,3 +10,13 @@
         }
     }
 }
+
+
+//## Exercise 2:
+
+//Use the list of [Linq methods] (https://portal.truecoders.io/course/1/lecture/62/lesson) for help
+
+//+Create a list of video game names...
+//+ Order the list of games by length of the game name.
+//+ Use the lambda expression in this exercise as well.
+//+ use Method Syntax for this exercise
